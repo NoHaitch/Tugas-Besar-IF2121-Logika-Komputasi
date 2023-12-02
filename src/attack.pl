8751a5796db6de09bc9df7ab339fa0dc470487e2
shazyaaa <shazyaaudrea@gmail.com>
2023-12-02 23:23:01 +0700

    modified:   src/attack.pl

--- src/attack.pl
+++ src/attack.pl
@@ -1,85 +1,59 @@
 /* FAKTA */
-neighbors(NA1, [NA2, NA3], ['NA2', 'NA3']).
-neighbors(NA2, [NA1, NA4, NA5], ['NA1', 'NA4', 'NA5']).
-neighbors(NA3, [SA1, NA4], ['SA1', 'NA4']).
-neighbors(NA4, [NA2, NA5], ['NA2', 'NA5']).
-neighbors(NA5, [NA2, NA4, E1], ['NA2', 'NA4', 'E1']).
+neighbors(na1, [na2, na3], ['na2', 'na3']).
+neighbors(na2, [na1, na4, na5], ['na1', 'na4', 'na5']).
+neighbors(na3, [sa1, na4], ['sa1', 'na4']).
+neighbors(na4, [na2, na5], ['na2', 'na5']).
+neighbors(na5, [na2, na4, e1], ['na2', 'na4', 'e1']).
 
-neighbors(SA1, [NA3, SA2], ['NA3', 'SA2']).
-neighbors(SA2, [SA1, AF1], ['SA1', 'AF1']).
+neighbors(sa1, [na3, sa2], ['na3', 'sa2']).
+neighbors(sa2, [sa1, af1], ['sa1', 'af1']).
 
-neighbors(E1, [NA5, E2, E3], ['NA5', 'E2', 'E3']).
-neighbors(E2, [E1, E4, A1], ['E1', 'E4', 'A1']).
-neighbors(E3, [E1, E4, AF1], ['E1', 'E4', 'AF1']).
-neighbors(E4, [E2, E3, AF2, E5], ['E2', 'E3', 'AF2', 'E5']).
-neighbors(E5, [E4, AF2, A4], ['E4', 'AF2', 'A4']).
+neighbors(e1, [na5, e2, e3], ['na5', 'e2', 'e3']).
+neighbors(e2, [e1, e4, a1], ['e1', 'e4', 'a1']).
+neighbors(e3, [e1, e4, af1], ['e1', 'e4', 'af1']).
+neighbors(e4, [e2, e3, af2, e5], ['e2', 'e3', 'af2', 'e5']).
+neighbors(e5, [e4, af2, a4], ['e4', 'af2', 'a4']).
 
-neighbors(A1, [A4, E2], ['A4', 'E2']).
-neighbors(A2, [A4, A5, A6], ['A4', 'A5', 'A6']).
-neighbors(A3, [A5], ['A5']).
-neighbors(A4, [E5, A1, A2, A5, A6], ['E5', 'A1', 'A2', 'A5', 'A6']).
-neighbors(A5, [A3, A4, A6], ['A3', 'A4', 'A6']).
-neighbors(A6, [A2, A4, A5, A7, AU1], ['A2', 'A4', 'A5', 'A7', 'AU1']).
-neighbors(A7, [A6], ['A6']).
+neighbors(a1, [a4, e2], ['a4', 'e2']).
+neighbors(a2, [a4, a5, a6], ['a4', 'a5', 'a6']).
+neighbors(a3, [a5], ['a5']).
+neighbors(a4, [e5, a1, a2, a5, a6], ['e5', 'a1', 'a2', 'a5', 'a6']).
+neighbors(a5, [a3, a4, a6], ['a3', 'a4', 'a6']).
+neighbors(a6, [a2, a4, a5, a7, au1], ['a2', 'a4', 'a5', 'a7', 'au1']).
+neighbors(a7, [a6], ['a6']).
 
-neighbors(AU1, [AU2, A6], ['AU2', 'A6']).
-neighbors(AU2, [A1], ['A1']).
+neighbors(au1, [au2, a6], ['au2', 'a6']).
+neighbors(au2, [a1], ['a1']).
 
-neighbors(AF1, [E3, AF2, AF3, SA2], ['E3', 'AF2', 'AF3', 'SA2']).
-neighbors(AF2, [E4, E5, AF1, AF3], ['E4', 'E5', 'AF1', 'AF3']).
-neighbors(AF3, [AF1, AF2], ['AF1', 'AF2']).
+neighbors(af1, [e3, af2, af3, sa2], ['e3', 'af2', 'af3', 'sa2']).
+neighbors(af2, [e4, e5, af1, af3], ['e4', 'e5', 'af1', 'af3']).
+neighbors(af3, [af1, af2], ['af1', 'af2']).
 
-:- dynamic(pemilik/2). /*
-pemilik('NA1', ' ').
-pemilik('NA2', ' ').
-pemilik('NA3', ' ').
-pemilik('NA4', ' ').
-pemilik('NA5', ' ').
-pemilik('E1', ' ').
-pemilik('E2', ' ').
-pemilik('E3', ' ').
-pemilik('E4', ' ').
-pemilik('E5', ' ').
-pemilik('A1', ' ').
-pemilik('A2', ' ').
-pemilik('A3', ' ').
-pemilik('A4', ' ').
-pemilik('A5', ' ').
-pemilik('A6', ' ').
-pemilik('A7', ' ').
-pemilik('AU1', ' ').
-pemilik('AU2', ' ').
-pemilik('AF1', ' ').
-pemilik('AF2', ' ').
-pemilik('AF3', ' ').
-pemilik('SA1', ' ').
-pemilik('SA2', ' '). */
-
-:- dynamic(totalTroops/2).
-totalTroops('NA1', 0).
-totalTroops('NA2', 0).
-totalTroops('NA3', 0).
-totalTroops('NA4', 0).
-totalTroops('NA5', 0).
-totalTroops('E1', 0).
-totalTroops('E2', 0).
-totalTroops('E3', 0).
-totalTroops('E4', 0).
-totalTroops('E5', 0).
-totalTroops('A1', 0).
-totalTroops('A2', 0).
-totalTroops('A3', 0).
-totalTroops('A4', 0).
-totalTroops('A5', 0).
-totalTroops('A6', 0).
-totalTroops('A7', 0).
-totalTroops('AU1', 0).
-totalTroops('AU2', 0).
-totalTroops('AF1', 0).
-totalTroops('AF2', 0).
-totalTroops('AF3', 0).
-totalTroops('SA1', 0).
-totalTroops('SA2', 0).
+:- dynamic(totalTentara/2).
+totalTentara('na1', 0).
+totalTentara('na2', 0).
+totalTentara('na3', 0).
+totalTentara('na4', 0).
+totalTentara('na5', 0).
+totalTentara('e1', 0).
+totalTentara('e2', 0).
+totalTentara('e3', 0).
+totalTentara('e4', 0).
+totalTentara('e5', 0).
+totalTentara('a1', 0).
+totalTentara('a2', 0).
+totalTentara('a3', 0).
+totalTentara('a4', 0).
+totalTentara('a5', 0).
+totalTentara('a6', 0).
+totalTentara('a7', 0).
+totalTentara('au1', 0).
+totalTentara('au2', 0).
+totalTentara('af1', 0).
+totalTentara('af2', 0).
+totalTentara('af3', 0).
+totalTentara('sa1', 0).
+totalTentara('sa2', 0).
 
 printList([], _) :- nl.
 printList([H | T], I) :- 
@@ -93,32 +67,40 @@ checkAttackableTerritory(X) :-
     neighbors(X, List, ListString), 
     printList(ListString, 1).
 
-battle(AttackerTerritory, DefenderTerritory, NumAttacker, Defender) :-
-    totalTroops(DefenderTerritory, NumDefender), nl,
-    currentPlayer(IDPlayer), player(IDPlayer, Attacker, TotalTerritories, TotalActiveTroops, TotalAddTroops, RiskCard),
-    (   
-        RiskCard =:= 2 -> 
-        write('Player '), write(Defender), write(' memiliki risk card DISEASE.~nSemua hasil lemparan dadu saat penyerangan dan pertahanan akan bernilai 1.~n'), nl
-    ),
+battle(IDPlayer, AttackerTerritory, DefenderTerritory, NumAttacker, NumDefender, Defender) :-
+    player(IDPlayer, Attacker, _, _, _, RiskCardAttacker),
+    player(_, Defender, _, _, _, RiskCardDefender),
 
-    (   
-        RiskCard =:= 5 ->
-        write('Player '), write(Attacker), write(' memiliki risk card SUPER SOLDIER SERUM.~nSemua hasil lemparan dadu saat penyerangan dan pertahanan akan bernilai 6.~n'), nl   
-    ),
-
-    throwDice(NumAttacker, AttackerDice),
+    throwDice(NumAttacker, AttackerDice, Attacker),
     (
-        RiskCard =:= 2 ; RiskCard =:= 5 ->
-        retract(player(IDPlayer, Attacker, TotalTerritories, TotalActiveTroops, TotalAddTroops, RiskCard)), asserta(player(IDPlayer, Attacker, NA, E, A, SA, AU, AF, TotalTerritories, TotalActiveTroops, TotalAddTroops, 0))
+        RiskCardAttacker =:= 2 ->
+        write('Player '), write(Attacker), write(' memiliki risk card DISEASE.~nSemua hasil lemparan dadu saat penyerangan dan pertahanan akan bernilai 1.~n'), nl,
+        retract(player(IDPlayer, Attacker, _, _, _, RiskCard)), asserta(player(_, Attacker, _, _, _, 0))
+    ;
+        RiskCardAttacker =:= 5 ->
+        write('Player '), write(Attacker), write(' memiliki risk card SUPER SOLDIER SERUM.~nSemua hasil lemparan dadu saat penyerangan dan pertahanan akan bernilai 6.~n'), nl,
+        retract(player(IDPlayer, Attacker, _, _, _, RiskCard)), asserta(player(_, Attacker, _, _, _, 0))
+    ;
+        write('')
     ),
     
-    pemilik(AttackerTerritory, Attacker),
     write('Player '), write(Attacker), nl,
     printDice(AttackerDice, 1),
     sumList(AttackerDice, AttackerTotal),
     write('Total: '), write(AttackerTotal), nl,
 
-    throwDice(NumDefender, DefenderDice),
+    throwDice(NumDefender, DefenderDice, Defender),
+    (
+        RiskCardDefender =:= 5 ->
+        write('Player '), write(Defender), format(' memiliki risk card DISEASE.~nSemua hasil lemparan dadu saat penyerangan dan pertahanan akan bernilai 1.~n'), nl,
+        retract(player(_, Defender, _, _, _, RiskCardAttacker)), asserta(player(_, Defender, _, _, _, 0))
+    ;
+        RiskCardDefender =:= 2 ->
+        write('Player '), write(Defender), format(' memiliki risk card SUPER SOLDIER SERUM.~nSemua hasil lemparan dadu saat penyerangan dan pertahanan akan bernilai 6.~n'), nl,
+        retract(player(_, Defender, _, _, _, RiskCardDefender)), asserta(player(_, Defender, _, _, _, 0))
+    ;
+        write('')
+    ),
     write('Player '), write(Defender), nl,
     printDice(DefenderDice, 1),
     sumList(DefenderDice, DefenderTotal),
@@ -126,27 +108,36 @@ battle(AttackerTerritory, DefenderTerritory, NumAttacker, Defender) :-
 
     (AttackerTotal > DefenderTotal ->
         format('Player ~w menang! Wilayah ~w sekarang dikuasai oleh Player ~w.~n', [Attacker, DefenderTerritory, Attacker]),
-        write('Silahkan tentukan banyaknya tentara yang menetap di wilayah ', DefenderTerritory, ': '), read(NewArmies),
-        moveArmies(AttackerTerritory, DefenderTerritory, NewArmies)
+        write('Silahkan tentukan banyaknya tentara yang menetap di wilayah '), write(DefenderTerritory), write(': '), read(NewArmies),
+        NewArmies > 1, 
+        NewArmies =< NumAttacker,
+        moveArmies(AttackerTerritory, DefenderTerritory, Attacker, Defender, NewArmies),
+        write('Tentara di wilayah '), write(AttackerTerritory), write(': '), write(AttackerArmies - NewArmies), nl,
+        write('Tentara di wilayah '), write(DefenderTerritory), write(': '), write(NewArmies), nl
     ;
-    format('Player ~w kalah! Sayang sekali penyerangan Anda gagal. Seluruh tentara yang dikirim hangus.~n', [Attacker])).
+        format('Player ~w kalah! Sayang sekali penyerangan Anda gagal. Seluruh tentara yang dikirim hangus.~n', [Attacker]), 
+        retract(totalTroops(AttackerTerritory, NumArmy)),
+        NewNumArmy is NumArmy - NumAttacker,
+        asserta(totalTroops(AttackerTerritory, NewNumArmy)),
+        write('Tentara di wilayah '), write(AttackerTerritory), write(': '), write(NewNumArmy), nl,
+        write('Tentara di wilayah '), write(DefenderTerritory), write(': '), write(NumDefender), nl
+    ).
 
-throwDice(0, []).
-throwDice(N, [Result | Rest]) :-
-    currentPlayer(IDPlayer), player(IDPlayer, Attacker, TotalTerritories, TotalActiveTroops, TotalAddTroops, RiskCard),
-    (
+throwDice(0, [], Owner).
+throwDice(N, [Result | Rest], Owner) :-
+    player(_, Owner, _, _, _, RiskCard),
+    (   
+        RiskCard =:= 5 -> 
+        Result is 1
+    ;
         RiskCard =:= 2 ->
         Result is 6
+    ;
+        random(1, 7, Hasil),
+        Result is Hasil
     ),
-
-    (
-        RiskCard =:= 5 ->
-        Result is 1
-    ),
-
-    random(1, 7, Result),
     N1 is N - 1,
-    throwDice(N1, Rest).
+    throwDice(N1, Rest, Owner).
 
 printDice([], _) :- nl.
 printDice([H | T], I) :-
@@ -157,13 +148,13 @@ sumList([], 0).
 sumList([X], X).
 sumList([X|List], Sum):- sumList(List, S1), Sum is (S1+X).
 
-moveArmies(AttackerTerritory, DefenderTerritory, NumArmies, Attacker, Defender) :-
+moveArmies(AttackerTerritory, DefenderTerritory, Attacker, Defender, NewArmies) :-
     retract(pemilik(DefenderTerritory, Defender)),
     retract(totalTroops(AttackerTerritory, AttackerArmies)),
     retract(totalTroops(DefenderTerritory, DefenderArmies)),
     
-    NewAttackerArmies is AttackerArmies - NumArmies,
-    NewDefenderArmies is NumArmies,
+    NewAttackerArmies is AttackerArmies - NewArmies,
+    NewDefenderArmies is NewArmies,
     
     asserta(pemilik(DefenderTerritory, Attacker)),
     asserta(totalTroops(AttackerTerritory, NewAttackerArmies)),
@@ -173,16 +164,6 @@ moveArmies(AttackerTerritory, DefenderTerritory, NumArmies, Attacker, Defender) 
 
 count([], 0).
 count([_|List], Count):- count(List, C1), Count is (C1+1).
-
-validTerritory(AttackerTerritory, List) :-
-    repeat,
-    write('Pilihlah daerah yang ingin Anda mulai untuk melakukan penyerangan: '),
-    read(Input),
-    (   \+ nonmember(Input, List) ->
-        AttackerTerritory is Input, nl
-    ;   write('Daerah tidak valid. Silahkan input kembali.'), nl,
-        fail
-    ).
 
 validArmy(NumAttacker, NumArmy) :-
     repeat,
@@ -194,31 +175,61 @@ validArmy(NumAttacker, NumArmy) :-
         fail
     ).
 
-validOption(Pilihan, List) :-
-    repeat,
-    write('Pilih: '),
-    read(Input),
-    count(List, Count),
-    (   number(Input), Input >= 1, Input =< Count ->
-        Pilihan is Input, nl
-    ;   write('Input tidak valid. Silahkan input kembali.'), nl,
-        fail
-    ).
-
 attack :- 
     currentPlayer(IDPlayer), player(IDPlayer, Attacker, TotalTerritories, TotalActiveTroops, TotalAddTroops, RiskCard),
-    write('Sekarang giliran Player '), write(Attacker), write('untuk menyerang.'), nl,
-    write('Pilihlah daerah yang ingin Anda mulai untuk melakukan penyerangan: '), read(AttackerTerritory), 
+    write('Sekarang giliran Player '), write(Attacker), write(' untuk menyerang.'), nl,
+    displayMap,
+    repeat,
+    write('Pilihlah daerah yang ingin Anda mulai untuk melakukan penyerangan: '),
+    read(AttackerTerritory),
+    pemilik(AttackerTerritory, Owner),
+    (   Owner \== Attacker ->
+        write('Daerah tidak valid. Silahkan input kembali.'), nl,
+        fail
+    ;
+        write('')
+    ),
     totalTroops(AttackerTerritory, NumArmy), NumArmy > 1, 
-    write('Dalam daerah '), write(AttackerTerritory), write(', Anda memiliki sebanyak '), write(NumArmy), write(' tentara.'),
+    write('Dalam daerah '), write(AttackerTerritory), write(', Anda memiliki sebanyak '), write(NumArmy), write(' tentara.'), nl,
 
     validArmy(NumAttacker, NumArmy),
+    write('Player '), write(Attacker), write(' mengirim sebanyak '), write(NumAttacker), write(' tentara untuk berperang.'), nl,
 
+    displayMap,
     write('Pilihlah daerah yang ingin Anda serang: \n'),
     checkAttackableTerritory(AttackerTerritory),
     neighbors(AttackerTerritory, List, ListString),
-    validOption(Pilihan, List),
+    repeat,
+    write('Pilih: '),
+    read(Input),
+    count(ListString, Count),
+    (
+        number(Input), Input >= 1, Input =< Count ->
+        nth1(Input, ListString, Territory),
+        pemilik(Territory, Owner1),
+        player(_, Owner1, _, _, _, A),
+        (
+            Owner1 \== Attacker ->
+            (
+                A =:= 1 ->
+                write('Tidak bisa menyerang! Wilayah ini dalam pengaruh CEASEFIRE ORDER.'), nl,
+                retract(player(_, Owner1, _, _, _, A)), asserta(player(_, Owner1, _, _, _, 0)),
+                fail
+            ;
+                Pilihan is Input, nl, !
+            )
+        ;
+            write('Anda Tidak Bisa Menyerang Diri Sendiri.'), nl,
+            fail
+        )
+    ;
+        write('Input tidak valid. Silahkan input kembali.'), nl,
+        fail
+    ),
     nth1(Pilihan, ListString, DefenderTerritory),
+    pemilik(Territory, Defender),
+    totalTroops(DefenderTerritory, NumDefender),
 
-    write('Perang telah dimulai.'),
-    battle(AttackerTerritory, DefenderTerritory, NumAttacker).
+    write('Perang telah dimulai.'), nl,
+    battle(IDPlayer, AttackerTerritory, DefenderTerritory, NumAttacker, NumDefender, Defender),
+    endTurn.
--- src/risk.pl
+++ src/risk.pl
@@ -15,7 +15,8 @@ risk :-
     currentPlayer(IDPlayer),
     player(IDPlayer, NamePlayer, TotalTerritories, TotalActiveTroops, TotalAddTroops, RiskCard),
     card(N, Card),
-    riskCard(Card, IDPlayer).
+    riskCard(Card, IDPlayer),
+    endTurn.
 
 
 riskCard(cease, Player) :- ceasefire(Player).
@@ -27,43 +28,37 @@ riskCard(sup, Player) :- supplychain(Player).
 
 
 ceasefire(P) :-
-    retract(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, [Bool, _, _, _, _, _])),
-    Bool is 1,
-    assertz(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, [Bool, _, _, _, _, _])),
+    retract(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, RiskCard)),
+    assertz(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, 1)),
     format('Player ~w mendapatkan risk card CEASEFIRE ORDER.~nHingga giliran berikutnya, wilayah pemain tidak dapat diserang oleh lawan.~n', [Name]).
      /* Player menjadi kebal untuk satu putaran */
 
 /* Semua hasil lemparan dadu player untuk turn selanjutnya selalu 6*/
 superSoldier(P) :- 
-    retract(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, [_, Bool, _, _, _, _])),
-    Bool is 1,
-    assertz(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, [_, Bool, _, _, _, _])),
+    retract(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, RiskCard)),
+    assertz(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, 2)),
     format('Player ~w mendapatkan risk card SUPER SOLDIER SERUM.~nHingga giliran berikutnya, semua hasil lemparan dadu saat penyerangan dan pertahanan akan bernilai 6.~n', [Name]).
 
 /* Jumlah tentara yang ditambahkan menjadi 2x kali lipatnya */
 auxiliary(P) :- 
-    retract(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, [_, _, Bool, _, _, _])),
-    Bool is 1,
-    assertz(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, [_, _, Bool, _, _, _])),
+    retract(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, RiskCard)),
+    assertz(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, 3)),
     format('Player ~w mendapatkan risk card AUXILIARY TROOPS.~nPada giliran berikutnya, tentara tambahan yang didapatkan pemain akan bernilai 2 kali lipat.~n', [Name]).
 
 /* Salah satu wilayah yang dikuasai akan menjadi milik lawan (acak) */
 rebelion(P) :- 
-    retract(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, [_, _, _, Bool, _, _])),
-    Bool is 1,
-    assertz(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, [_, _, _, Bool, _, _])),
+    retract(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, RiskCard)),
+    assertz(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, 4)),
     format('Player ~w mendapatkan risk card REBELION.~nSalah satu wilayah acak pemain akan berpindah kekuasaan menjadi milik lawan.~n', [Name]).
 
 /* Semua hasil lemparan dadu player untuk turn selanjutnya akan selalu bernilai 1 */
 disease(P) :- 
-    retract(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, [_, _, _, _, Bool, _])),
-    Bool is 1,
-    assertz(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, [_, _, _, _, Bool, _])),
-    format('Player ~w mendapatkan risk card SUPER SOLDIER SERUM.~nHingga giliran berikutnya, wilayah pemain tidak dapat diserang oleh lawan.~n', [Name]).
+    retract(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, RiskCard)),
+    assertz(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, 5)),
+    format('Player ~w mendapatkan risk card DISEASE.~nHingga giliran berikutnya, wilayah pemain tidak dapat diserang oleh lawan.~n', [Name]).
 
 /* Jumlah tentara tambahan akan 0 di turn selanjutnya */
 supplychain(P) :- 
-    retract(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, [_, _, _, _, _, Bool])),
-    Bool is 1,
-    assertz(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, [_, _, _, _, _, Bool])),
-    format('Player ~w mendapatkan risk card SUPER SOLDIER SERUM.~nHingga giliran berikutnya, wilayah pemain tidak dapat diserang oleh lawan.~n', [Name]).
+    retract(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, RiskCard)),
+    assertz(player(ID, Name, TotalTerritories, TotalActiveTroops, TotalAddTroops, 6)),
+    format('Player ~w mendapatkan risk card SUPPLY CHAIN.~nHingga giliran berikutnya, wilayah pemain tidak dapat diserang oleh lawan.~n', [Name]).
